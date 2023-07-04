ecb5892892887d89bf8a90e8d86bc16382eea867
codedecode25 <codergaming25@gmail.com>
2023-07-04 15:25:17 +0530

    test cases added

--- src/test/java/com/codedecode/foodcatalogue/FoodCatalogueMicroserviceApplicationTests.java
+++ src/test/java/com/codedecode/foodcatalogue/FoodCatalogueMicroserviceApplicationTests.java
@@ -1,3 +1,4 @@
+/*
 package com.codedecode.foodcatalogue;
 
 import org.junit.jupiter.api.Test;
@@ -11,3 +12,4 @@ class FoodCatalogueMicroserviceApplicationTests {
 	}
 
 }
+*/
---
+++ src/test/java/com/codedecode/foodcatalogue/service/FoodCatalogueServiceTest.java
@@ -0,0 +1,72 @@
+package com.codedecode.foodcatalogue.service;
+
+import com.codedecode.foodcatalogue.dto.FoodCataloguePage;
+import com.codedecode.foodcatalogue.dto.FoodItemDTO;
+import com.codedecode.foodcatalogue.dto.Restaurant;
+import com.codedecode.foodcatalogue.entity.FoodItem;
+import com.codedecode.foodcatalogue.mapper.FoodItemMapper;
+import com.codedecode.foodcatalogue.repo.FoodItemRepo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class FoodCatalogueServiceTest {
+
+    @Mock
+    private FoodItemRepo foodItemRepo;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private FoodCatalogueService foodCatalogueService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void addFoodItem_ShouldSaveFoodItemAndReturnMappedDTO() {
+        // Arrange
+        FoodItemDTO foodItemDTO = new FoodItemDTO();
+        FoodItem foodItem = new FoodItem();
+        when(foodItemRepo.save(any(FoodItem.class))).thenReturn(foodItem);
+
+        // Act
+        FoodItemDTO result = foodCatalogueService.addFoodItem(foodItemDTO);
+
+        // Assert
+        verify(foodItemRepo, times(1)).save(any(FoodItem.class));
+        Assertions.assertEquals(FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDto(foodItem), result);
+    }
+
+    @Test
+    void fetchFoodCataloguePageDetails_ShouldReturnFoodCataloguePage() {
+        // Arrange
+        int restaurantId = 123;
+        List<FoodItem> foodItemList = Arrays.asList(new FoodItem());
+        Restaurant restaurant = new Restaurant();
+        when(foodItemRepo.findByRestaurantId(restaurantId)).thenReturn(foodItemList);
+        when(restTemplate.getForObject(anyString(), eq(Restaurant.class))).thenReturn(restaurant);
+
+        // Act
+        FoodCataloguePage result = foodCatalogueService.fetchFoodCataloguePageDetails(restaurantId);
+
+        // Assert
+        verify(foodItemRepo, times(1)).findByRestaurantId(restaurantId);
+        verify(restTemplate, times(1)).getForObject(anyString(), eq(Restaurant.class));
+        Assertions.assertEquals(foodItemList, result.getFoodItemsList());
+        Assertions.assertEquals(restaurant, result.getRestaurant());
+    }
+}
